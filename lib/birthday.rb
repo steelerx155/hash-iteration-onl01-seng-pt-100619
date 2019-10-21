6521bbc0f41fe31cd80fad001b51cde78305246e
Randy Steele <steelerx155@gmail.com>
2019-10-21 14:49:50 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -15,5 +15,9 @@ def age_appropriate_birthday(birthday_kids)
   birthday_kids.each do |kids_name, age|
     puts "Happy Birtday #{kids_name}! You are now #{age} years old!"
     if age > 6 
-      puts "Y"
+      puts "You are too old for this."
+    end
+end
+
+
 
